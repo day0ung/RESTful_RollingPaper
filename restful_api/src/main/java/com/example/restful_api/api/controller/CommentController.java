30e29c0d2cf3a9dd81f5493da884ef67fc525325
day0ung <jdy9436@naver.com>
2023-07-27 04:28:00 +0900

    chore : 도메인별 controller 생성

---
+++ restful_api/src/main/java/com/example/restful_api/api/controller/CommentController.java
@@ -0,0 +1,7 @@
+package com.example.restful_api.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/commnet")
+public class CommentController {
+}
