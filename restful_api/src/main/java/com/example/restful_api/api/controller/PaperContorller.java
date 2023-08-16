4fc915eaa5fbc27324b53a9679ff94a977cca16c
day0ung <jdy9436@naver.com>
2023-08-16 16:43:11 +0900

    feat : Paper 생성/수정/삭제 기능 추가

--- restful_api/src/main/java/com/example/restful_api/api/controller/PaperContorller.java
+++ restful_api/src/main/java/com/example/restful_api/api/controller/PaperContorller.java
@@ -2,16 +2,15 @@ package com.example.restful_api.api.controller;
 
 
 import com.example.restful_api.api.BaseResponse;
-import com.example.restful_api.api.dto.paper.PaperResponse;
 import com.example.restful_api.api.dto.paper.PaperPostRequest;
+import com.example.restful_api.api.dto.paper.PaperPutRequest;
+import com.example.restful_api.api.dto.paper.PaperResponse;
 import com.example.restful_api.service.PaperService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,10 +21,24 @@ public class PaperContorller {
     private final PaperService paperService;
 
     @PostMapping
-    public BaseResponse<PaperResponse> createPaper(@RequestBody @Validated PaperPostRequest paperPostRequest){
-        PaperResponse result = paperService.save(paperPostRequest);
-        return BaseResponse.setSuccess(result);
-
+    public ResponseEntity<BaseResponse<PaperResponse>> createPaper(@RequestBody PaperPostRequest request){
+        PaperResponse response = paperService.createPaper(request);
+        return new ResponseEntity<>(BaseResponse.setSuccess(response), HttpStatus.CREATED);
     }
+
+    @PutMapping({"/{paperId}"})
+    public ResponseEntity<BaseResponse<PaperResponse>> updatePaper(@PathVariable("paperId") Long paperId,
+                                                                   @RequestBody PaperPutRequest request){
+        PaperResponse response = paperService.updatePaper(paperId ,request);
+        return new ResponseEntity<>(BaseResponse.setSuccess(response), HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping({"/{paperId}"})
+    public ResponseEntity<BaseResponse<PaperResponse>> deletePaper(@PathVariable("paperId") Long paperId){
+        PaperResponse response = paperService.deletePaper(paperId);
+        return new ResponseEntity<>(BaseResponse.setSuccess(response), HttpStatus.OK);
+    }
+
 
 }
--- restful_api/src/main/java/com/example/restful_api/service/PaperService.java
+++ restful_api/src/main/java/com/example/restful_api/service/PaperService.java
@@ -1,8 +1,11 @@
 package com.example.restful_api.service;
 
 import com.example.restful_api.api.dto.paper.PaperPostRequest;
+import com.example.restful_api.api.dto.paper.PaperPutRequest;
 import com.example.restful_api.api.dto.paper.PaperResponse;
+import com.example.restful_api.domain.papers.Paper;
 import com.example.restful_api.domain.papers.PaperRepository;
+import com.example.restful_api.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,8 +16,37 @@ public class PaperService {
     private final PaperRepository paperRepository;
 
     @Transactional
-    public PaperResponse save(PaperPostRequest requestDto) {
-        return new PaperResponse(paperRepository.save(requestDto.toEntity()).getId());
+    public PaperResponse createPaper(PaperPostRequest request) {
+        Paper paper = paperRepository.save(request.toEntity());
+        return new PaperResponse(paper);
     }
+
+    @Transactional
+    public PaperResponse updatePaper(Long paperId , PaperPutRequest request) {
+        Paper paper = verifyPost(paperId);
+
+        paper.updateTitle(request.getTitle());
+        if (!request.getTitle().isEmpty()) paper.updateContent(request.getContent());
+
+        return new PaperResponse(paper);
+    }
+
+    @Transactional
+    public PaperResponse deletePaper(Long paperId) {
+        Paper paper = paperRepository.findById(paperId).orElseThrow(
+                () -> new ResourceNotFoundException(Paper.class.getSimpleName(),"paperId", paperId)
+        );
+        paperRepository.delete(paper);
+
+        return new PaperResponse(paper);
+    }
+
+    private Paper verifyPost(Long paperId) {
+        Paper paper = paperRepository.findById(paperId).orElseThrow(
+                () -> new ResourceNotFoundException(Paper.class.getSimpleName(), "paperId", paperId)
+        );
+        return paper;
+    }
+
 
 }
