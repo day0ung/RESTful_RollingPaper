86c80232e52795f0a246b4b90a9ecab5fd397907
day0ung <jdy9436@naver.com>
2023-08-16 17:04:37 +0900

    refactor :  오타수정

--- restful_api/src/main/java/com/example/restful_api/api/controller/UserController.java
+++ restful_api/src/main/java/com/example/restful_api/api/controller/UserController.java
@@ -2,9 +2,13 @@ package com.example.restful_api.api.controller;
 
 import com.example.restful_api.api.BaseResponse;
 import com.example.restful_api.api.dto.user.UserPostRequest;
+import com.example.restful_api.api.dto.user.UserResponse;
 import com.example.restful_api.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
@@ -13,9 +17,9 @@ public class UserContoller {
 
     private final UserService userService;
 
-    @PostMapping("/signin")
+    @PostMapping("/signup")
     public BaseResponse<?> signUp(@RequestBody UserPostRequest userPostRequest) throws Exception {
-        userService.save(userPostRequest);
+        UserResponse result = userService.signUp(requestBody);
         return BaseResponse.setSuccess("ok");
     }
 }
